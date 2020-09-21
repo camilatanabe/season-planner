d31298744be86a9bc847f046d46aefe88c38d74d
Mila <camila_tanabe@hotmail.com>
2020-09-21 20:28:09 -0300

    Display date in drawer

--- src/MainPage/index.js
+++ src/MainPage/index.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import Calendar from '../components/Calendar'
-import { Container, Drawer } from './styles'
+import * as dateFns from 'date-fns'
+import { Container, Drawer, Day, Month } from './styles'
 
 const MainPage = () => {
   const [selectedDate, setSelectedDate] = useState(new Date())
   const [isOpenDrawer, setIsOpenDrawer] = useState(false)
+  const formattedDay = dateFns.format(selectedDate, 'dd')
+  const formattedMonth = dateFns.format(selectedDate, 'MMMM')
 
   const openDaySchedule = day => {
     setSelectedDate(day)
@@ -12,10 +15,19 @@ const MainPage = () => {
     return console.log('aqui: ', day)
   }
 
+  const ScheduleDrawer = () => {
+    return (
+      <Drawer>
+        <Day>{formattedDay}</Day>
+        <Month>{formattedMonth}</Month>
+      </Drawer>
+    )
+  }
+
   return (
     <Container>
       <Calendar selectedDate={selectedDate} daySchedule={openDaySchedule} />
-      {isOpenDrawer && <Drawer />}
+      {isOpenDrawer && ScheduleDrawer()}
     </Container>
   )
 }
--- src/MainPage/styles.js
+++ src/MainPage/styles.js
@@ -3,8 +3,22 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: flex;
   flex-direction: row;
+  margin: 5%;
 `
 
 export const Drawer = styled.div`
-  padding: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 40%;
+  padding-left: 40px;
+  margin-left: 5%;
+`
+export const Day = styled.p`
+  font-size: 26px;
+`
+
+export const Month = styled.p`
+  font-size: 20px;
+  margin: 0;
 `
